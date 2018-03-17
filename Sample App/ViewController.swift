2d980bc5f9923641e1af3757c8d889937856710a
Stefan Renne <stefan.renne@opinity.nl>
2018-03-17 09:50:44 +0100

    Setup project structure

---
+++ Sample App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample App
+//
+//  Created by Stefan Renne on 17/03/2018.
+//  Copyright © 2018 Uberweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
