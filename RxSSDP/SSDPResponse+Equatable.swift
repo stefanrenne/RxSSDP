d7ea5fa4057419e82517e6ef836538ce22240af1
Stefan Renne <info@stefanrenne.nl>
2018-03-18 09:24:31 +0100

    Added public init for SSDPResponse for enabling the mocking of SSDPRepository

--- RxSSDP/SSDPResponse+Equatable.swift
+++ RxSSDP/SSDPResponse+Equatable.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 extension SSDPResponse: Equatable {
+    
+    public convenience init(data: [String: String]) {
+        self.init(dictionary: data)
+    }
+    
     public static func ==(lhs: SSDPResponse, rhs: SSDPResponse) -> Bool {
         return lhs.description == rhs.description
     }
