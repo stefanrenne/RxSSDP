934ac8f24e60b4026a4d741bfbc5ad43ddcb2ed4
Stefan Renne <info@stefanrenne.nl>
2018-04-03 15:17:57 +0200

    Added mac & tvos support + upgraded to swift 4.1

--- Pods/Target Support Files/Pods-Sample App/Pods-Sample App-frameworks.sh
+++ Pods/Target Support Files/Pods-Sample App/Pods-Sample App-frameworks.sh
@@ -134,12 +134,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS11.2/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS9.0/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS11.2/RxSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-iOS9.0/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
 fi
